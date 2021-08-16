b1d17a07098b917bd4aa69c8b9ef975df88ffbcc
Salah-Zair <salahedinemdr15@gmail.com>
2021-08-16 19:01:21 +0100

    add website

---
+++ website/js/request.js
@@ -0,0 +1,149 @@
+var temp_data =
+{"class_rooms":[{"id":1,"name":"Amphi 5","extid":"","capacity":1000,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":3,"name":"Grande Salle 12","extid":"","capacity":120,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":8,"name":"Salle 36","extid":"","capacity":40,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":10,"name":"Salle 40","extid":"","capacity":40,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":12,"name":"Salle 44","extid":"","capacity":40,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":16,"name":"Salle TP 2","extid":"","capacity":35,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":17,"name":"Salle TP 3","extid":"","capacity":35,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":20,"name":"Salle-p 41","extid":"","capacity":40,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}}],"courses":[{"id":1,"name":"Alg2","short_name":"Algo2","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":30,"extid":"","to_hold_together_with":[103,18,35,52,69,86,120],"related_group":1},{"id":2,"name":"Alg2","short_name":"Algo2-G1","course_type":"TD","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":24,"extid":"","to_hold_together_with":[],"related_group":1},{"id":3,"name":"Alg2","short_name":"Algo2-G1","course_type":"tp","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":44,"extid":"11","to_hold_together_with":[],"related_group":1},{"id":4,"name":"Algèbre 2","short_name":"Alg2","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":49,"extid":"","to_hold_together_with":[21,38,55,72,88,106,123],"related_group":1},{"id":5,"name":"Algèbre 2","short_name":"Alg2-G1","course_type":"TD","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":49,"extid":"","to_hold_together_with":[],"related_group":1},{"id":6,"name":"Analyse 2","short_name":"Anal2","course_type":"Cours","num_of_lessons_per_week":2,"num_of_enrolled_students":35,"group_name":"","teacher_id":4,"extid":"","to_hold_together_with":[23,40,57,74,91,108,125],"related_group":1},{"id":7,"name":"Analyse 2","short_name":"Anal2-G1","course_type":"TD","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":4,"extid":"","to_hold_together_with":[],"related_group":1},{"id":8,"name":"Anglais","short_name":"Anglais 1","course_type":"cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":37,"extid":"","to_hold_together_with":[25,42,59,76,93,110,127],"related_group":1},{"id":9,"name":"Outils de porgrammation pour les maths","short_name":"OPM","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":50,"extid":"","to_hold_together_with":[26,43,60,77,94,111,128],"related_group":1},{"id":10,"name":"Outils de programmation pour les Maths","short_name":"OPM-G1","course_type":"TP","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":50,"extid":"","to_hold_together_with":[],"related_group":1},{"id":11,"name":"Physique 2","short_name":"Physique 2","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":56,"extid":"","to_hold_together_with":[28,45,62,79,96,113,130],"related_group":1},{"id":12,"name":"Physique 2","short_name":"Physique 2-G1","course_type":"TD","num_of_lessons_per_week":1,"num_of_enrolled_students":25,"group_name":"","teacher_id":41,"extid":"","to_hold_together_with":[],"related_group":1},{"id":13,"name":"Probabilité","short_name":"Prob","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":16,"extid":"","to_hold_together_with":[30,47,64,81,98,115,132],"related_group":1},{"id":14,"name":"Probabilité","short_name":"Prob_G1","course_type":"TD","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":16,"extid":"","to_hold_together_with":[],"related_group":1},{"id":15,"name":"Structure machine","short_name":"STRM-G1","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":25,"group_name":"","teacher_id":55,"extid":"","to_hold_together_with":[32,49,66,83,100,117,134],"related_group":1},{"id":16,"name":"Structure machine","short_name":"STRM-G1","course_type":"TD","num_of_lessons_per_week":1,"num_of_enrolled_students":25,"group_name":"","teacher_id":55,"extid":"","to_hold_together_with":[],"related_group":1},{"id":17,"name":"TIC","short_name":"TIC","course_type":"Cours","num_of_lessons_per_week":1,"num_of_enrolled_students":35,"group_name":"","teacher_id":22,"extid":"","to_hold_together_with":[34,68,51,85,102,119,136],"related_group":1}],"teachers":[{"id":4,"name":"Nabil","last_name":"Ait Yala","title":"Mr","edu_rank":"MAB","extid":null},{"id":16,"name":"Madjid","last_name":"Birouche","title":"Mr","edu_rank":"MAA","extid":null},{"id":22,"name":"H","last_name":"Bouglimina","title":"Mme","edu_rank":"MAB","extid":null},{"id":24,"name":"Farida","last_name":"Brahimi","title":"Mme","edu_rank":"MAB","extid":null,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":4,"term_index":1},{"day_index":4,"term_index":2},{"day_index":4,"term_index":3},{"day_index":4,"term_index":4},{"day_index":4,"term_index":5},{"day_index":4,"term_index":6}]}},{"id":30,"name":"Abderrazak","last_name":"Djouabri","title":"Mr","edu_rank":"MAA","extid":null,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}},{"id":37,"name":"Dalila","last_name":"Idir","title":"Melle","edu_rank":"Vac","extid":null},{"id":41,"name":"Abderrazak","last_name":"Merriche","title":"Mr","edu_rank":"MAA","extid":null},{"id":44,"name":"Rokia","last_name":"Ouadi Merabet","title":"M","edu_rank":"Vac","extid":null},{"id":49,"name":"Souad","last_name":"Rafaa","title":"Mme","edu_rank":"MAA","extid":null},{"id":50,"name":"Nessrine","last_name":"Smaili","title":"Mme","edu_rank":"Vac","extid":null},{"id":55,"name":"Taha","last_name":"Zerrouki","title":"Mr","edu_rank":"MAA","extid":null},{"id":56,"name":"Hachemi","last_name":"Zitouni","title":"Mr","edu_rank":"MAA","extid":null,"spec_slots":{"type":"unallowed","spec_slots":[{"day_index":1,"term_index":4},{"day_index":2,"term_index":4},{"day_index":3,"term_index":4},{"day_index":4,"term_index":4},{"day_index":5,"term_index":4},{"day_index":6,"term_index":4}]}}],"group":{"id":1,"name":"1MI 01","code":"G01","semester":"S2"},"lessons":[{"course_id":1,"day_index":1,"term_index":2,"classroom_id":1},{"course_id":2,"day_index":1,"term_index":3,"classroom_id":12},{"course_id":3,"day_index":5,"term_index":5,"classroom_id":16},{"course_id":4,"day_index":3,"term_index":1,"classroom_id":1},{"course_id":5,"day_index":3,"term_index":3,"classroom_id":20},{"course_id":6,"day_index":4,"term_index":1,"classroom_id":1},{"course_id":6,"day_index":3,"term_index":2,"classroom_id":1},{"course_id":7,"day_index":3,"term_index":5,"classroom_id":8},{"course_id":8,"day_index":1,"term_index":1,"classroom_id":1},{"course_id":9,"day_index":6,"term_index":2,"classroom_id":1},{"course_id":10,"day_index":6,"term_index":5,"classroom_id":17},{"course_id":11,"day_index":5,"term_index":2,"classroom_id":1},{"course_id":12,"day_index":4,"term_index":3,"classroom_id":20},{"course_id":13,"day_index":6,"term_index":1,"classroom_id":1},{"course_id":14,"day_index":6,"term_index":3,"classroom_id":10},{"course_id":15,"day_index":5,"term_index":1,"classroom_id":1},{"course_id":16,"day_index":4,"term_index":2,"classroom_id":3},{"course_id":17,"day_index":2,"term_index":1,"classroom_id":1}],"days":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6}],"terms":[{"index":1,"term":"08:00-09:30"},{"index":2,"term":"09:30-11:00"},{"index":3,"term":"11:00-12:30"},{"index":4,"term":"12:30-13:00"},{"index":5,"term":"13:00-14:30"},{"index":6,"term":"14:30-16:00"}]}
+
+// START POINT
+$(document).ready(function () {
+    build_table();
+});
+
+
+
+function build_table() {
+    let table = $('#schedule');
+
+    row_length = temp_data.terms.length + 1;
+    column_length = temp_data.days.length + 1;
+
+    for (var row = 0; row < row_length; row++) {
+        column_raw = ""
+        for (var column = 0; column < column_length; column++) {
+            if (row == 0 && column == 0) {
+                column_raw += getEmptyHeader()
+            } else if (row == 0) {
+                column_raw += getCellDay(temp_data.days[column - 1]);
+            } else if (column == 0) {
+                column_raw += getTerm(temp_data.terms[row - 1]);
+            } else {
+                column_raw += get_lesson_cell(row, column);
+            }
+        }
+        row_raw = getRow(column_raw, row);
+        table.append(row_raw);
+    }
+}
+
+
+
+
+
+function getEmptyCell() {
+    return `<td></td>`
+}
+
+function getEmptyHeader() {
+    return `<th></th>`
+}
+
+function getCellDay(day_data) {
+    return `<th data-index='${day_data.id}'>${day_data.id}</th>`
+}
+
+function getTerm(term) {
+    return `<td data-index='${term.index}'>${term.term}</td>`
+}
+
+function getRow(inner_data, row_number) {
+    return `<tr>${inner_data}</tr>`
+}
+
+
+
+function get_lesson_cell(day_index, term_index) {
+    var data = ""
+    temp_data.lessons.forEach(element => {
+        if (element.day_index == day_index && element.term_index == term_index) {
+            course = get_course_data(temp_data.courses, element.course_id);
+            group = temp_data.group;
+            teacher = get_course_data(temp_data.teachers, course.teacher_id);
+            class_room = get_class_room(temp_data.class_rooms, element.classroom_id);
+            console.log(temp_data.courses);
+
+
+            inner_data = `<div class='course-item-data ${course.course_type.toLocaleLowerCase()}'>`;
+        
+
+            inner_data += `<div class='course-data'>`;
+            inner_data += `<a href='#'>`;
+            inner_data += `${course.short_name}`;
+            inner_data += `</a>`;
+            inner_data += `</div>`;
+
+            inner_data += `<div class='class-room-data'>`;
+            inner_data += `<a href='#'>`;
+            inner_data += `${class_room.name}`;
+            inner_data += `</a>`;
+            inner_data += `</div>`;
+
+            inner_data += `<div class='teacher-data'>`;
+            inner_data += `<a href='#'>`;
+            inner_data += `${teacher.name} ${teacher.last_name}`;
+            inner_data += `</a>`;
+            inner_data += `</div>`;
+
+            inner_data += `</div>`;
+
+            data = `<td> ${inner_data} </td>`;
+        }
+    });
+
+    if (data)
+        return data;
+
+    return "<td></td>"
+}
+
+function get_course_data(courses, course_id) {
+    let course;
+    courses.forEach(course_item => {
+        if (course_item.id == course_id) {
+            course = course_item
+        }
+    })
+    return course
+}
+
+
+function get_group_data(groups, group_id) {
+    let group;
+    groups.forEach(group_item => {
+        if (group_item.id == group_id) {
+            group = group_item
+        }
+    })
+    return group;
+}
+
+
+function get_teacher_data(teachers, teacher_id) {
+    let teacher;
+    teachers.forEach(teacher_item => {
+        if (teacher_item.id == teacher_id) {
+            teacher = teacher_item
+        }
+    })
+    return teacher;
+}
+
+
+function get_class_room(class_rooms, class_room_id) {
+    let class_room;
+    class_rooms.forEach(class_room_item => {
+        if (class_room_item.id == class_room_id) {
+            class_room = class_room_item
+        }
+    })
+
+    return class_room;
+}
+
+
